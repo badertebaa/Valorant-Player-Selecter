a1c95210a8b5291b800e36baa3b5e02504beaab3
unknown <badertebaa2@gmail.com>
2023-09-29 20:37:13 +0100

    fix sova uuid

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ const agents = [{'name':"astra",'id':"41fb69c1-4189-7b37-f117-bcaf1e96f1bf"},
                {'name':"skye","id":"6f2a04ca-43e0-be17-7f36-b3908627744d"}, 
                {'name':"viper","id":"707eab51-4836-f488-046a-cda6bf494859"}, 
                {'name':"yoru","id":"7f94d92c-4234-0a36-9646-3a87eb8b5c89"},
-               {'name':"sova","id":"ded3520f-4264-bfed-162d-b080e2abccf9"}
+               {'name':"sova","id":"320b2a48-4d9b-a075-30f1-1f93a9b638fa"}
             ]
 
 const selectAgent = document.getElementById("selectedAgent")
@@ -37,13 +37,13 @@ function getAgentId(id) {
     const url = "https://valorant-api.com/v1/agents/"+id;
     fetch(url)
       .then((response) => response.json())
-      .then((data) => updatePlayerInfo(data,id)).catch((error) => {
+      .then((data) => updatePlayerInfo(data)).catch((error) => {
         console.error("Error fetching agent data:", error);
       });
   }
   
-  function updatePlayerInfo(data,id) {
-    if(id!="ded3520f-4264-bfed-162d-b080e2abccf9"){
+  function updatePlayerInfo(data) {
+   
     const roleNameElement = document.getElementById("role-name");
     roleNameElement.innerHTML = data.data.role.displayName;
     document.getElementById("agent-name").innerHTML=data.data.displayName;
@@ -71,35 +71,8 @@ function getAgentId(id) {
       document.getElementById("player-and-abilities-info").innerHTML=data.data.abilities[3].description;
     })
 
-    }
-    else{
-      document.getElementById("role-name").innerHTML="Initiator"
-      document.getElementById("agent-name").innerHTML="Sova";
-      document.getElementById("info-player").setAttribute("src","https://media.valorant-api.com/agents/roles/1b47567f-8f7b-444b-aae3-b0c634622d10/displayicon.png");
-      document.getElementById("Abilitie1").setAttribute("src",data.data.abilities[1].displayIcon);
-      document.getElementById("Abilitie2").setAttribute("src",data.data.abilities[0].displayIcon);
-      document.getElementById("Abilitie3").setAttribute("src",data.data.abilities[2].displayIcon);
-      document.getElementById("Abilitie4").setAttribute("src",data.data.abilities[3].displayIcon);
-      document.getElementById("player-and-abilities-info").innerHTML=data.data.description;
-      document.getElementById("mini-title-role-player").innerHTML="Initiator";
-      document.getElementById("role-text").innerHTML="Initiators challenge angles by setting up their team to enter contested ground and push defenders away.";
-      document.getElementById("info-player").addEventListener("click",()=>{
-        document.getElementById("player-and-abilities-info").innerHTML=data.data.description;
-        })
-        document.getElementById("Abilitie1").addEventListener("click",()=>{
-          document.getElementById("player-and-abilities-info").innerHTML=data.data.abilities[1].description;
-        })
-        document.getElementById("Abilitie2").addEventListener("click",()=>{
-          document.getElementById("player-and-abilities-info").innerHTML=data.data.abilities[0].description;
-        })
-        document.getElementById("Abilitie3").addEventListener("click",()=>{
-          document.getElementById("player-and-abilities-info").innerHTML=data.data.abilities[2].description;
-        })
-        document.getElementById("Abilitie4").addEventListener("click",()=>{
-          document.getElementById("player-and-abilities-info").innerHTML=data.data.abilities[3].description
-        })
-      
-    }
+    
+    
   }
 
   window.addEventListener("load",()=>{
